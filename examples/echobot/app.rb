e5c3e13de02e04ad612b64a9e01261ea6ea49390
cutter1020 <58689319+cutter1020@users.noreply.github.com>
2023-09-12 14:32:09 +0700

    Update app.rb

--- examples/echobot/app.rb
+++ examples/echobot/app.rb
@@ -37,7 +37,7 @@ post '/callback' do
             type: 'text',
             text: event.message['text']
           }
-          client.reply_message(event['replyToken'], message)
+          #client.reply_message(event['replyToken'], message)
           #client.push_message("U97f1978ea01a7f94867501b8a66b6038", message)
           #client
          # Publish example
